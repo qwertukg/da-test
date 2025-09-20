8c9940bf133f6daf16c9731772b15cf4b61ae715
daniil <daniil.rakhmatulin@gmail.com>
2025-09-20 08:50:20 +0500

    removes saving

--- src/run.py
+++ src/run.py
@@ -35,7 +35,7 @@ def load_mnist_28x28(train_limit=1000, test_limit=200, seed=0):
 
 def rr_init(app_name: str = "digits-layout", spawn: bool = True, class_labels=None):
     rr.init(app_name, spawn=spawn)
-    rr.save("../out/rks.rrd")
+    # rr.save("../out/rks.rrd")
     if class_labels is not None:
         ann = [AnnotationInfo(id=int(i), label=str(lbl)) for i, lbl in class_labels.items()]
         rr.log("layout", rr.AnnotationContext(ann), static=True)
@@ -142,8 +142,8 @@ def run() -> None:
     enc.print_keyhole_records(True)
 
     lay = Layout2D(
-        R_far=64, epochs_far=500,
-        R_near=3, epochs_near=0,
+        R_far=64, epochs_far=100,
+        R_near=3, epochs_near=100,
         seed=123
     )
 
